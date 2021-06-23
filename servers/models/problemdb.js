0e5ca4b1217d5cd0a64bc0f1246b1fb29c061ab0
KeisLuv5991 <chmhj9997@gmail.com>
2021-06-23 22:42:35 +0900

    fix Elevator file name bug

--- servers/models/problemdb.js
+++ servers/models/problemdb.js
@@ -3,76 +3,106 @@ const problemdb_config = require('../config/problemdb_config.json');
 
 const uri = problemdb_config.uri;
 const MongoClient = require('mongodb').MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+let coll = null;
 exports.MongoClient = MongoClient;
+
+
+exports.connect = async function () {
+    await MongoClient.connect();
+    coll = MongoClient.db("document").collection("problem");
+};
+
+
+exports.close = async function () {
+    await MongoClient.close();
+};
+
+
+exports.create = async (id, data) => {
+    try {
+        const result = await coll.insertOne({ _id: id, data });
+        console.log(result);
+        return result;
+    }
+    catch (err) {
+        console.log(err);
+        return null;
+    }
+};
+
+exports.update = async (id, data) => {
+    try {
+        const coll = MongoClient.db("document").collection("problem");
+        const result = await coll.updateOne({ _id: id }, { $set: { data } });
+        console.log(result);
+        return result;
+    }
+    catch (err) {
+        console.log(err);
+        return null;
+    }
+};
+
+exports.find_by_problemId = async (id) => {
+    try {
+        const coll = MongoClient.db("document").collection("problem");
+        const result = await coll.find({ _id: id }).toArray();
+        console.log(result);
+        return result[0];
+    }
+    catch (err) {
+        console.log(err);
+        return null;
+    }
+};
+
+exports.delete = async (id) => {
+    try {
+        const coll = MongoClient.db("document").collection("problem");
+        const result = await coll.deleteOne({ _id: id });
+        console.log(result);
+        return result;
+    }
+    catch (err) {
+        console.log(err);
+        return null;
+    }
+
+};
+
+
+
+
+
+
+
+
+
+// ========================================================== testing
+
+
+/*
 const fs = require('fs');
 
-let data = fs.readFileSync('../../docs/API.md', 'utf-8');
-console.log(data);
+let api = fs.readFileSync('./docs/API.md', 'utf-8');
+let description = fs.readFileSync('./docs/ELEVATOR.md', 'utf-8');
 
 async function connect_doc() {
     await MongoClient.connect();
 }
 
-exports.connect = async function () {
-    await MongoClient.connect();
-};
 
-
-exports.close = async function (req, res, next) {
-    await MongoClient.close();
-    next();
-};
-
-
-/*
-let result = await MongoClient.db("gradingDB").collection("problem_id0").
-    insertOne({ _id: token, gradingData, entire_tickets });
-const result = await MongoClient.db("gradingDB").collection("problem_id0").
-    updateOne({ _id: token }, { $set: { gradingData, entire_tickets } });
-
-
-const result = await MongoClient.db("gradingDB").collection("problem_id0").
-    find({ _id: token }).toArray();
-return result[0];
-
-
-            const result = await MongoClient.db("gradingDB").collection("problem_id0").
-                find({}).toArray();
-
-
-            const myQuery = { "_id": token };
-            const result = await MongoClient.db("gradingDB").collection("problem_id0").
-                deleteOne(myQuery);
-*/
-
-async function create_doc(data) {
+async function ps(api, description) {
     await connect_doc();
 
     const coll = MongoClient.db("document").collection("problem");
-    let result = await coll.insertOne({ _id: "elevator", data });
+    let result = await coll.insertOne({ _id: "elevator", api, description});
+    // let result = await coll.find({ _id: id }).toArray();
     console.log(result);
     return result;
 }
 
 
-let ret = create_doc(data);
+let ret = ps(api, description);
 
-
-
-
-exports.create = async (data) => {
-
-
-};
-
-exports.update = async (id, data) => {
-
-};
-
-exports.find_by_problemId = async (id) => {
-
-};
-
-exports.delete = async (token) => {
-
-};
+*/
